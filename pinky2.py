2c6429e8e44c854db9dee9058bfc68d359e32183
jprosfira <jpros4600@gmail.com>
2025-12-01 14:44:49 +0700

    update

--- pinky2.py
+++ pinky2.py
@@ -11,7 +11,7 @@ from decimal import Decimal, InvalidOperation
 import resend
 import requests
 
-
+#terbaru
 # ============================================================
 # 🔹 Setup Logging
 # ============================================================
